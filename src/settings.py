00ab995c6d136df5f59cdfdfcccf0b54a1f8de8c
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-06-22 21:35:29 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ RELEASE_YEAR: int = 2020
 HOMEPAGE_URL: str = "https://saras-esad.grand-challenge.org/Home/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 740115
+PREVIEW_IMAGE_ID: int = 695814
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/saras-esad-2020"
