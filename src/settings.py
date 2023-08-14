43a84ed7bb96eb870935eb043282712129d5b454
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-14 19:34:02 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "SARAS-ESAD 2020"
 PROJECT_NAME_FULL: str = "SARAS Endoscopic Vision Challenge for Surgeon Action Detection 2020"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -120,6 +121,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,        
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
