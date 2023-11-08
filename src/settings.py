d754b61d042603ac24705246c5f830fa5c5bccd2
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-08 22:40:46 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 LICENSE: License = License.CC_BY_NC_SA_3_0(source_url="https://saras-esad.grand-challenge.org/download/")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical(), Industry.Robotics()]
 CATEGORY: Category = Category.Medical(
-    benchmark=True, extra=Category.Robotics(), sensitive_content=True
+   extra=Category.Robotics(), sensitive_content=True
 )
 
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
